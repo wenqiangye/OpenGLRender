2cc24d949cc345fcc9cf603a16ac46d14278f667
yesky <a316606581@gmail.com>
2023-07-14 14:47:43 +0800

    初始化 imgui框架

---
+++ build/CMakeFiles/openglrender.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yesky/code/OpenGLProjects/OpenGLRender/OpenGLRender/imgui/backends/imgui_impl_glfw.cpp" "/home/yesky/code/OpenGLProjects/OpenGLRender/OpenGLRender/build/CMakeFiles/openglrender.dir/imgui/backends/imgui_impl_glfw.cpp.o"
+  "/home/yesky/code/OpenGLProjects/OpenGLRender/OpenGLRender/imgui/backends/imgui_impl_opengl3.cpp" "/home/yesky/code/OpenGLProjects/OpenGLRender/OpenGLRender/build/CMakeFiles/openglrender.dir/imgui/backends/imgui_impl_opengl3.cpp.o"
+  "/home/yesky/code/OpenGLProjects/OpenGLRender/OpenGLRender/imgui/imgui.cpp" "/home/yesky/code/OpenGLProjects/OpenGLRender/OpenGLRender/build/CMakeFiles/openglrender.dir/imgui/imgui.cpp.o"
+  "/home/yesky/code/OpenGLProjects/OpenGLRender/OpenGLRender/imgui/imgui_demo.cpp" "/home/yesky/code/OpenGLProjects/OpenGLRender/OpenGLRender/build/CMakeFiles/openglrender.dir/imgui/imgui_demo.cpp.o"
+  "/home/yesky/code/OpenGLProjects/OpenGLRender/OpenGLRender/imgui/imgui_draw.cpp" "/home/yesky/code/OpenGLProjects/OpenGLRender/OpenGLRender/build/CMakeFiles/openglrender.dir/imgui/imgui_draw.cpp.o"
+  "/home/yesky/code/OpenGLProjects/OpenGLRender/OpenGLRender/imgui/imgui_tables.cpp" "/home/yesky/code/OpenGLProjects/OpenGLRender/OpenGLRender/build/CMakeFiles/openglrender.dir/imgui/imgui_tables.cpp.o"
+  "/home/yesky/code/OpenGLProjects/OpenGLRender/OpenGLRender/imgui/imgui_widgets.cpp" "/home/yesky/code/OpenGLProjects/OpenGLRender/OpenGLRender/build/CMakeFiles/openglrender.dir/imgui/imgui_widgets.cpp.o"
+  "/home/yesky/code/OpenGLProjects/OpenGLRender/OpenGLRender/src/unix/main.cpp" "/home/yesky/code/OpenGLProjects/OpenGLRender/OpenGLRender/build/CMakeFiles/openglrender.dir/src/unix/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "IMGUI_IMPL_OPENGL_LOADER_GL3W"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../imgui"
+  "../imgui/backends"
+  "../imgui/examples/libs/gl3w"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/openglrender.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "../bin/openglrender"
+  "../bin/openglrender.pdb"
+  "CMakeFiles/openglrender.dir/imgui/backends/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/openglrender.dir/imgui/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/openglrender.dir/imgui/imgui.cpp.o"
+  "CMakeFiles/openglrender.dir/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/openglrender.dir/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/openglrender.dir/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/openglrender.dir/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/openglrender.dir/src/unix/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/openglrender.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
